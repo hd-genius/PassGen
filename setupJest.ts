e22da57bef603eab5a205c8222c484fe351e5b21
Roy Stewart <roythomasstewart@gmail.com>
2022-08-10 06:37:07 -0400

    Added comment explaining the crypto test polyfill

--- setupJest.ts
+++ setupJest.ts
@@ -1,6 +1,7 @@
 import 'jest-preset-angular';
 
 import * as nodeCrypto from 'crypto';
+// Polyfill for jsdom
 Object.defineProperty(global, 'crypto', {
     value: {
         getRandomValues: buffer => nodeCrypto.randomFillSync(buffer)
